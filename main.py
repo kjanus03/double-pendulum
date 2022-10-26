0fdca542e47d8d65ef3737d32142ccb1f3f1fdd9
Kacper Janus <kacperj07@icloud.com>
2022-10-26 18:10:18 +0200

    Added working resizability

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
+import math
+
 import numpy as np
 import pygame as pg
 
 from pendulum import Pendulum
 
 
-def update_accelerations(Pendulum1, Pendulum2):
-    g = 0.8
-    num1_1 = -g * (2 * Pendulum1.m + Pendulum2.m) * np.sin(Pendulum1.a)
-    num1_2 = -Pendulum2.m * g * np.sin(Pendulum1.a - 2 * Pendulum2.a)
-    num1_3 = -2 * np.sin(Pendulum1.a - Pendulum2.a) * Pendulum2.m
-    num1_4 = Pendulum2.av ** 2 * Pendulum2.r + Pendulum1.av ** 2 * Pendulum1.r * np.cos(Pendulum1.a - Pendulum2.a)
+def update_accelerations(pendulum1, pendulum2, g):
+    two_pi = 2 * np.pi
+    num1_1 = -g * (2 * pendulum1.m + pendulum2.m) * np.sin(pendulum1.a, dtype=np.float64)
+    num1_2 = -pendulum2.m * g * np.sin(pendulum1.a - 2 * pendulum2.a, dtype=np.float64)
+    num1_3 = -2 * np.sin(pendulum1.a - pendulum2.a, dtype=np.float64) * pendulum2.m
+    num1_4 = pendulum2.av ** 2 * pendulum2.r + pendulum1.av ** 2 * pendulum1.r * np.cos(pendulum1.a - pendulum2.a,
+                                                                                        dtype=np.float64)
     num1 = num1_1 + num1_2 + num1_3 * num1_4
-    den = Pendulum1.r * (2 * Pendulum1.m + Pendulum2.m - Pendulum2.m * np.cos(2 * Pendulum1.a - 2 * Pendulum2.a))
+    den = pendulum1.r * (2 * pendulum1.m + pendulum2.m - pendulum2.m * np.cos(2 * pendulum1.a - 2 * pendulum2.a,
+                                                                              dtype=np.float64))
 
-    num2_1 = 2 * np.sin(Pendulum1.a - Pendulum2.a)
-    num2_2 = Pendulum1.av ** 2 * Pendulum1.r * (Pendulum1.m + Pendulum2.m) + g * (
-            Pendulum1.m + Pendulum2.m) * np.cos(
-        Pendulum1.a) + Pendulum2.av ** 2 * Pendulum2.r * Pendulum2.m * np.cos(Pendulum1.a - Pendulum2.a)
+    num2_1 = 2 * np.sin(pendulum1.a - pendulum2.a, dtype=np.float64)
+    num2_2 = pendulum1.av ** 2 * pendulum1.r * (pendulum1.m + pendulum2.m) + g * (
+            pendulum1.m + pendulum2.m) * np.cos(
+        pendulum1.a, dtype=np.float64) + pendulum2.av ** 2 * pendulum2.r * pendulum2.m * np.cos(
+        pendulum1.a - pendulum2.a, dtype=np.float64)
 
-    Pendulum1.aa = num1 / den
-    Pendulum2.aa = (num2_1 * num2_2) / den
+    pendulum1.aa = num1 / den
+    pendulum2.aa = (num2_1 * num2_2) / den
+    if abs(pendulum1.aa) > two_pi:
+        pendulum1.aa %= two_pi
+    if abs(pendulum2.aa) > two_pi:
+        pendulum2.aa %= two_pi
 
 
-def game_loop(Pendulum1, Pendulum2, size, screen_colour, y_offset, fps, trail_point_size=1):
+def game_loop(pendulum1, pendulum2, size, screen_colour, y_offset, fps, g=1, trail_point_size=1):
     pg.init()
     screen = pg.display.set_mode(size, pg.RESIZABLE)
     background = pg.Surface(size, pg.SRCALPHA, 32)
@@ -40,26 +49,28 @@ def game_loop(Pendulum1, Pendulum2, size, screen_colour, y_offset, fps, trail_po
         w, h = pg.display.get_surface().get_size()
         middle = w / 2
         background = pg.transform.scale(background, (w, h))
-        pg.draw.circle(background, Pendulum2.col,
-                       (Pendulum1.x + Pendulum2.x + middle, Pendulum1.y + Pendulum2.y + y_offset), trail_point_size)
+        pg.draw.circle(background, pendulum2.col,
+                       (pendulum1.x + pendulum2.x + middle, pendulum1.y + pendulum2.y + y_offset), trail_point_size)
 
-        pg.draw.line(screen, Pendulum1.col, (middle, y_offset), (Pendulum1.x + middle, Pendulum1.y + y_offset))
-        pg.draw.circle(screen, Pendulum1.col, (Pendulum1.x + middle, Pendulum1.y + y_offset), Pendulum1.m)
+        pg.draw.line(screen, pendulum1.col, (middle, y_offset), (pendulum1.x + middle, pendulum1.y + y_offset))
+        pg.draw.circle(screen, pendulum1.col, (pendulum1.x + middle, pendulum1.y + y_offset),
+                       math.pow(pendulum1.m, 1 / 3) * 3)
 
-        pg.draw.line(screen, Pendulum2.col, (Pendulum1.x + middle, Pendulum1.y + y_offset),
-                     (Pendulum1.x + Pendulum2.x + middle, Pendulum1.y + Pendulum2.y + y_offset))
-        pg.draw.circle(screen, Pendulum2.col,
-                       (Pendulum1.x + Pendulum2.x + middle, Pendulum1.y + Pendulum2.y + y_offset), Pendulum2.m)
+        pg.draw.line(screen, pendulum2.col, (pendulum1.x + middle, pendulum1.y + y_offset),
+                     (pendulum1.x + pendulum2.x + middle, pendulum1.y + pendulum2.y + y_offset))
+        pg.draw.circle(screen, pendulum2.col,
+                       (pendulum1.x + pendulum2.x + middle, pendulum1.y + pendulum2.y + y_offset),
+                       math.pow(pendulum2.m, 1 / 3) * 3)
 
-        update_accelerations(Pendulum1, Pendulum2)
+        update_accelerations(pendulum1, pendulum2, g)
 
-        Pendulum1.update_velocity()
-        Pendulum1.update_angle()
-        Pendulum1.update_position()
+        pendulum1.update_velocity()
+        pendulum1.update_angle()
+        pendulum1.update_position()
 
-        Pendulum2.update_velocity()
-        Pendulum2.update_angle()
-        Pendulum2.update_position()
+        pendulum2.update_velocity()
+        pendulum2.update_angle()
+        pendulum2.update_position()
 
         pg.time.wait(1000 // fps)
         screen.blit(background, (0, 0))
@@ -71,18 +82,19 @@ def game_loop(Pendulum1, Pendulum2, size, screen_colour, y_offset, fps, trail_po
 def main():
     starting_angle1 = np.random.choice(np.linspace(0, 2 * np.pi, 40))
     starting_angle2 = np.random.choice(np.linspace(0, 2 * np.pi, 40))
-    r1, m1, a1, col1 = 120, 20, starting_angle1, (255, 255, 255)
-    r2, m2, a2, col2 = 120, 20, starting_angle2, (221, 66, 232)
+    r1, m1, a1, col1 = 120, 40, starting_angle1, (255, 255, 255)
+    r2, m2, a2, col2 = 120, 40, starting_angle2, (221, 66, 232)
 
-    Pendulum1 = Pendulum(r1, m1, a1, col1)
-    Pendulum2 = Pendulum(r2, m2, a2, col2)
+    pendulum1 = Pendulum(r1, m1, a1, col1)
+    pendulum2 = Pendulum(r2, m2, a2, col2)
 
-    size = (800, 600)
-    screen_colour = (20, 20, 20)
-    y_offset = 210
-    fps = 60
+    x, y = 800, 600
+    size = (x, y)
+    screen_colour = (34, 34, 34)
+    y_offset = (y-r1)/2
+    fps = 40
 
-    game_loop(Pendulum1, Pendulum2, size, screen_colour, y_offset, fps)
+    game_loop(pendulum1, pendulum2, size, screen_colour, y_offset, fps, g=1)
 
 
 if __name__ == '__main__':
--- pendulum.py
+++ pendulum.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+
 class Pendulum:
+    two_pi = 2 * np.pi
+
     def __init__(self, r, m, a, col, av=0, aa=0):
         self.r = r
         self.m = m
@@ -13,9 +16,16 @@ class Pendulum:
 
     def update_velocity(self):
         self.av += self.aa
+        if abs(self.av) > self.two_pi:
+            self.av %= self.two_pi
 
     def update_angle(self):
         self.a += self.av
+        if abs(self.a) > self.two_pi:
+            self.a %= self.two_pi
+
+    def to_string(self):
+        return f'Angle: {self.a}\nAngular velocity: {self.av}\nAngular acceleration: {self.aa}\n\n'
 
     def update_position(self):
         self.x = self.r * np.sin(self.a)
